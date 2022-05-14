c7d1da29befd0a69414e8224c6950088b62fafe3
Mikail Sonmez <mikail4458@hotmail.com>
2022-05-14 13:11:23 +0300

    v8

---
+++ day05/JDBC.sql
@@ -0,0 +1,47 @@
+CREATE TABLE bolumler (
+      bolum_id   int PRIMARY KEY,
+      bolum_isim VARCHAR(14),
+      konum      VARCHAR(13)
+    );
+    
+    INSERT INTO bolumler VALUES (10,'MUHASEBE','IST');
+    INSERT INTO bolumler VALUES (20,'MUDURLUK','ANKARA');
+    INSERT INTO bolumler VALUES (30,'SATIS','IZMIR');
+    INSERT INTO bolumler VALUES (40,'ISLETME','BURSA');
+    INSERT INTO bolumler VALUES (50,'DEPO', 'YOZGAT');
+    
+    commit;
+
+    CREATE TABLE personel (
+      id   int PRIMARY KEY,
+      isim VARCHAR(10),
+      meslek        VARCHAR(9),
+      mudur_id      int,
+      maas          int,
+      bolum_id      int
+    );
+    
+ 
+  
+    INSERT INTO personel VALUES (7369,'AHMET','KATIP',1111,800,20);
+    INSERT INTO personel VALUES (7499,'BAHATTIN','SATISE',1222,1600,30);
+    INSERT INTO personel VALUES (7521,'NESE','SATISE',1222,1250,30);
+    INSERT INTO personel VALUES (1111,'MUZAFFER','MUDUR',7839,2975,20);
+    INSERT INTO personel VALUES (7654,'MUHAMMET','SATISE',1222,1250,30);
+    INSERT INTO personel VALUES (1222,'EMINE','MUDUR',7839,2850,30);
+    INSERT INTO personel VALUES (1333,'HARUN','MUDUR',7839, 2450,10);
+    INSERT INTO personel VALUES (7788,'MESUT','ANALIST',1111,3000,20);
+    INSERT INTO personel VALUES (7839,'SEHER','BASKAN',NULL,5000,10);
+    INSERT INTO personel VALUES (7844,'DUYGU','SATISE',1222,1500,30);
+    INSERT INTO personel VALUES (7876,'ALI','KATIP',1111,1100,20);
+    INSERT INTO personel VALUES (7900,'MERVE','KATIP',1222,950,30);
+    INSERT INTO personel VALUES (7902,'NAZLI','ANALIST',1111,3000,20);
+    INSERT INTO personel VALUES (7934,'EBRU','KATIP',1333,1300,10);
+    INSERT INTO personel VALUES (7956,'SIBEL','MIMAR',1333,3300,60);
+    INSERT INTO personel VALUES (7933,'ZEKI','MUHENDIS',1333,4300,60);
+    
+	commit;
+    
+    select * from personel;
+    select * from bolumler;
+	select * from urunler;
